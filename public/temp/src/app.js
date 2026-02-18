a152df613f056f4fdbd9eb303f72f32ad0938c6b
Swagath Somanna <swagathsomanna1718@gmail.com>
2026-02-18 18:07:14 +0530

    controllers and middlewares

--- public/temp/src/app.js
+++ public/temp/src/app.js
@@ -9,4 +9,17 @@ app.use(cors({
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: '16kb'}));
 app.use(express.static("public"));
+
+
+//routes
+import userRoutes from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRoutes);
+
+//http://localhost:8000/api/v1/users/register
+
+
 export default app;
+
+
